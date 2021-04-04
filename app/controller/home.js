5ac3e2f844db4e1ae339f35906b7ed122a1ee14f
1329778364 <1329778364@qq.com>
2021-04-04 17:46:03 +0800

    发生的f

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,9 +2,11 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
     async index() {
-        this.ctx.body = 'Hello world';
+        this.ctx.body = 'Hellasasdas dasd> o world<div>asd s</div>';
+        this.ctx.response.etag = "sad"
+
     }
-    // shdofh oascascascascehich  ds sdg sdgsdg 
+    // shdofh oascascascascehich  ds sdg sdgsdg  asfasfasfasfasfihsdf
 }
 
 module.exports = HomeController;
